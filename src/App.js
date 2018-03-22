1d07f7c660b45212e793a6dd8c00e7e5c7e3f3fb
yongsuk.jung <yongsuk.jung@linefriends.com>
2018-03-22 18:54:01 +0900

    react-route 적용

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const links = [{ to: '/', text: 'Home' }, { to: '/about', text: 'About' }];
+
+const Home = () => (
+  <div>
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to React</h1>
+    </header>
+    <p className="App-intro">
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+  </div>
+);
+
+const About = () => (
+  <div>
+    <span>About</span>
+  </div>
+);
+
+const App = () => (
+  <div className="App">
+    <ul>
+      {links.map(link => (
+        <li>
+          <Link to={link.to} href={link.to}>
+            {link.text}
+          </Link>
+        </li>
+      ))}
+    </ul>
+
+    <hr />
+
+    <Route exact path="/" component={Home} />
+    <Route path="/about" component={About} />
+  </div>
+);
 
 export default App;
